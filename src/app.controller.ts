cd7c5ce21a17d2e36112634305b7416b3e7b93dc
zhongyic00 <zhong200234@outlook.com>
2021-08-25 23:43:11 +0800

    device register prototype

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -12,5 +13,18 @@ export class AppController {
   @Get('/logs')
   getLogs(): string[] {
     return this.appService.webLogger()
+  }
+  @Post('/device/register')
+  registerIP(@Body() req) {
+    try {
+      this.appService.updateIP(req.name,req.record)
+      console.log('register device',req)
+    } catch(e){
+      console.log(e)
+    }
+  }
+  @Get('/device/list')
+  getIP() {
+    return this.appService.getIPs()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { PORT } from './main'
 
 let logs:any[] = []
+const ipRecords = new Map<string,any>()
 @Injectable()
 export class AppService {
   getHello(): string {
@@ -16,5 +17,14 @@ export class AppService {
     return logs.reduce(
       (p,v,i) => p+`<li>${i}:${v}</li>`,'<ul>'
     )
+  }
+  updateIP(name:string,record:any) {
+    ipRecords.set(name,record)
+  }
+  getIPs() {
+    console.log(ipRecords)
+    let rt = []
+    ipRecords.forEach((v,k)=> rt.push({name:v,record:k}))
+    return rt
   }
 }
